7a608c285148473e0122bcb82e9d25f872a2204a
Jarl Holmberg Lindquist <jarllindquist@gmail.com>
2016-05-27 08:58:08 +0200

    removed two folders css and php

--- fetch/php/db_connector.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('db_server', 'webcrunch.se.mysql');
-define('db_username', 'webcrunch_se');
-define('db_password', 'Lb435S6S');
-define('db_database', 'webcrunch_se');
-$connection = mysql_connect(db_server, db_username, db_password) or die(mysql_error());
-$database = mysql_select_db(db_database) or die(mysql_error());
-?>
